da70583f07bfd5da5a251f772d78c56de8b5c6fd
Byron Henze <byronh@gmail.com>
2015-05-16 11:48:34 -0700

    Login undefer password

--- dashto/controllers/main.py
+++ dashto/controllers/main.py
@@ -3,6 +3,7 @@ from dashto.auth import Permissions
 from dashto.controllers.base import BaseController
 from dashto.models import DBSession, User
 from pyramid.view import view_config
+from sqlalchemy.orm import undefer
 
 
 class MainController(BaseController):
@@ -15,7 +16,7 @@ class MainController(BaseController):
     def login(self):
         form = forms.UserLoginForm(**self.form_kwargs)
         if self.validate(form):
-            user = DBSession.query(User).filter(User.name == form.user_name.data).first()
+            user = DBSession.query(User).options(undefer('password')).filter(User.name == form.user_name.data).first()
             if user and user.validate_password(form.user_password.data):
                 self.request.session['user_id'] = user.id
                 return self.redirect('home')
